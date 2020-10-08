96854b6eced8885660c69d7967db2359217942f2
Sam Horsfield <s.horsfield19@imperial.ac.uk>
2020-10-08 15:12:57 +0100

    Removes duplicate pygfa directory.
    Former-commit-id: 87beee07f9f05dce4a84ea0e61316a8174a323b9

--- pygfa/__init__.py
+++
@@ -1,3 +0,0 @@
-from pygfa import gfa
-from pygfa.operations import * # common operations
-from pygfa.dovetail_operations.operations import * # common operations
--- pygfa/graph_element/__init__.py
+++
@@ -1,4 +0,0 @@
-from pygfa.graph_element import node
-from pygfa.graph_element import edge
-from pygfa.graph_element import subgraph
-from pygfa.graph_element import parser
