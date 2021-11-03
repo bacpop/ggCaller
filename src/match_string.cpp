77fa1ab3f86f526b04c3e02deef106d0d9ff1292
Sam Horsfield <s.horsfield19@imperial.ac.uk>
2021-11-03 12:33:55 +0000

    Changes fm_index construction from file to string input.
    Former-commit-id: 07bb7f639e75b85029db0eb3a9a5f3bc95b08d3e

--- src/match_string.cpp
+++ src/match_string.cpp
@@ -37,7 +37,7 @@ fm_index_coll index_fasta(const std::string& fasta_file,
         kseq_destroy(seq);
         gzclose(fp);
 
-        sdsl::construct(ref_index, reference_seq, 1); // generate index
+        sdsl::construct_im(ref_index, reference_seq, 1); // generate index
         if (write_idx)
         {
             store_to_file(ref_index, idx_file_name); // save it
