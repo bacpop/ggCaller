eaec010aed12ce1a522772d9928b820468e99b2f
Sam Horsfield <s.horsfield19@imperial.ac.uk>
2022-03-30 17:46:48 +0100

    Adds hashing to networkx graph to determine new centroids during clustering and adds iterative edge adding after node addition.

--- panaroo_runner/generate_network.py
+++ panaroo_runner/generate_network.py
@@ -17,11 +17,13 @@ def generate_network(DBG, overlap, high_scoring_ORFs, high_scoring_ORF_edges, cl
         centroid_genome_id = ORF_list[0][0]
         centroid_local_id = ORF_list[0][1]
 
+        # hold current_hash
+        current_hash = 0
+
         # access ORF information for centroid from high_scoring_ORFs, ensuring cluster is present
         if centroid_local_id not in high_scoring_ORFs[centroid_genome_id]:
             # if centroid not present, go through ORF_list and find next largest ORF present
             current_length = 0
-            current_hash = 0
             for ORF_ID_pair in ORF_list:
                 genome_id = ORF_ID_pair[0]
                 local_id = ORF_ID_pair[1]
@@ -57,9 +59,15 @@ def generate_network(DBG, overlap, high_scoring_ORFs, high_scoring_ORF_edges, cl
         ORFNodeVector = high_scoring_ORFs[centroid_genome_id][centroid_local_id]
         pan_centroid_ID = str(centroid_genome_id) + "_0_" + str(centroid_local_id)
 
+        # generate hash for later comparisons if not already generated
+        if not current_hash:
+            seq = DBG.generate_sequence(ORFNodeVector[0], ORFNodeVector[1], overlap)
+            current_hash = hash(seq)
+
         # add information to cluster_centroid_data
         cluster_centroid_data[cluster_id] = {
             'ORF_info': ORFNodeVector,
+            'hash': current_hash,
             'annotation': '',
             'description': '',
         }
@@ -115,15 +123,6 @@ def generate_network(DBG, overlap, high_scoring_ORFs, high_scoring_ORF_edges, cl
             # map ORF ID to centroid ID
             seqid_to_centroid[ORF_id] = cluster_centroids[current_cluster]
 
-            # parse neighbour information for current ORF if present, otherwise make empty edge_set
-            if local_id in high_scoring_ORF_edges[genome_id]:
-                edge_set = high_scoring_ORF_edges[genome_id][local_id]
-                has_end = False
-            else:
-                edge_set = set()
-                # check if current ORF is end of contig
-                has_end = True
-
             # initialise cluster to add
             cluster_to_add = current_cluster
 
@@ -135,6 +134,8 @@ def generate_network(DBG, overlap, high_scoring_ORFs, high_scoring_ORF_edges, cl
                     centroid_context[
                         cluster_centroids[current_cluster]].append(
                         [cluster_to_add, genome_id])
+                    # overwrite seq_to_cluster for current ORF
+                    seq_to_cluster[ORF_id] = cluster_to_add
                 G.add_node(
                     cluster_to_add,
                     size=1,
@@ -142,9 +143,10 @@ def generate_network(DBG, overlap, high_scoring_ORFs, high_scoring_ORF_edges, cl
                     maxLenId=0,
                     members=intbitset([genome_id]),
                     seqIDs=set([ORF_id]),
-                    hasEnd=has_end,
+                    hasEnd=False,
                     ORF_info=[(cluster_centroid_data[current_cluster]['ORF_info'][0],
                                cluster_centroid_data[current_cluster]['ORF_info'][1])],
+                    hash=[cluster_centroid_data[current_cluster]['hash']],
                     annotation=cluster_centroid_data[current_cluster]
                     ['annotation'],
                     bitscore=0,
@@ -166,78 +168,35 @@ def generate_network(DBG, overlap, high_scoring_ORFs, high_scoring_ORF_edges, cl
                     G.nodes[current_cluster]['size'] += 1
                     G.nodes[current_cluster]['members'].add(genome_id)
                     G.nodes[current_cluster]['seqIDs'].add(ORF_id)
-                    if G.nodes[current_cluster]['hasEnd'] == False: G.nodes[current_cluster]['hasEnd'] = has_end
 
-            # iterate through edge_set, adding nodes and then adding edges if required
+    # Iterate over all nodes in graph, and add edges
+    for node in G.nodes():
+        for ORF_id in G.nodes[node]['seqIDs']:
+            parsed_id = ORF_id.split("_")
+            genome_id = int(parsed_id[0])
+            local_id = int(parsed_id[-1])
+
+            # if edge present between ORFs add, otherwise pass
+            if local_id in high_scoring_ORF_edges[genome_id]:
+                edge_set = high_scoring_ORF_edges[genome_id][local_id]
+            else:
+                # if not present, then assume at end of contig as only has single/no linkage
+                if G.nodes[node]['hasEnd'] == False: G.nodes[node]['hasEnd'] = True
+                continue
+
             for neighbour in edge_set:
-                # generate panaroo neighbour id
                 pan_neigbour_id = str(genome_id) + "_0_" + str(neighbour)
 
                 neighbour_cluster = seq_to_cluster[pan_neigbour_id]
 
-                # check that neighbour exists, if not add to graph
-                add_neighbour = True if not G.has_node(neighbour_cluster) else False
-
-                # check if current cluster contains paralogs
-                neighbour_has_paralogs = True if neighbour_cluster in paralogs else False
-
-                # initialise cluster to add
-                neighbour_cluster_to_add = neighbour_cluster
-
-                # parse neighbour information for current ORF
-                if neighbour in high_scoring_ORF_edges[genome_id]:
-                    neighbour_has_end = False
-                else:
-                    neighbour_has_end = True
-
-                # add neighbour if not present in graph already
-                if add_neighbour:
-                    if neighbour_has_paralogs:
-                        # create a new paralog
-                        n_nodes += 1
-                        neighbour_cluster_to_add = n_nodes
-                        centroid_context[
-                            cluster_centroids[neighbour_cluster]].append(
-                            [neighbour_cluster_to_add, genome_id])
-                    G.add_node(
-                        neighbour_cluster_to_add,
-                        size=1,
-                        centroid=[cluster_centroids[neighbour_cluster]],
-                        maxLenId=0,
-                        members=intbitset([genome_id]),
-                        seqIDs=set([pan_neigbour_id]),
-                        hasEnd=neighbour_has_end,
-                        ORF_info=[(cluster_centroid_data[neighbour_cluster]['ORF_info'][0],
-                                   cluster_centroid_data[neighbour_cluster]['ORF_info'][1])],
-                        annotation=cluster_centroid_data[neighbour_cluster]
-                        ['annotation'],
-                        bitscore=0,
-                        description=cluster_centroid_data[neighbour_cluster]
-                        ['description'],
-                        lengths=[
-                            cluster_centroid_data[neighbour_cluster]['ORF_info'][2]
-                        ],
-                        paralog=neighbour_has_paralogs,
-                        mergedDNA=False)
-                else:
-                    # check if pan_neigbour_id already added to the cluster
-                    if pan_neigbour_id not in G.nodes[neighbour_cluster]['seqIDs']:
-                        G.nodes[neighbour_cluster]['size'] += 1
-                        G.nodes[neighbour_cluster]['members'].add(genome_id)
-                        G.nodes[neighbour_cluster]['seqIDs'].add(pan_neigbour_id)
-                        if G.nodes[neighbour_cluster]['hasEnd'] == False:
-                            G.nodes[neighbour_cluster]['hasEnd'] = neighbour_has_end
-
                 # add edge between current ORF and neighbour
-                if G.has_edge(cluster_to_add, neighbour_cluster_to_add):
-                    G[cluster_to_add][neighbour_cluster_to_add]['size'] += 1
-                    G[cluster_to_add][neighbour_cluster_to_add]['members'].add(genome_id)
+                if G.has_edge(node, neighbour_cluster):
+                    G[node][neighbour_cluster]['size'] += 1
+                    G[node][neighbour_cluster]['members'].add(genome_id)
                 else:
-                    G.add_edge(cluster_to_add,
-                               neighbour_cluster_to_add,
+                    G.add_edge(node,
+                               neighbour_cluster,
                                size=1,
                                members=intbitset([genome_id]))
-
-                # currently don't add edge between current cluster paralog and neighbour paralog.
 
     return G, centroid_context, seqid_to_centroid
