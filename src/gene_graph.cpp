055fbfd91846f30f3d54aef8cfdf77a2aefbf2c1
Sam Horsfield <s.horsfield19@imperial.ac.uk>
2024-10-04 11:58:23 +0100

    Adds per-component transitive closure

--- src/gene_graph.cpp
+++ src/gene_graph.cpp
@@ -87,7 +87,8 @@ std::vector<size_t> getPath(
         const VertexDescriptor& source,
         const VertexDescriptor& destination,
         float& path_score,
-        const std::vector<size_t>& vertex_mapping)
+        const std::vector<size_t>& vertex_mapping,
+        const std::vector<size_t>& component_vertex_mapping)
 {
     std::vector<VertexDescriptor> path;
 
@@ -102,11 +103,11 @@ std::vector<size_t> getPath(
     VertexDescriptor current = destination;
     while (current != source)
     {
-        path.push_back(vertex_mapping.at(current));
+        path.push_back(vertex_mapping.at(component_vertex_mapping.at(current)));
         path_score += distances.at(current);
         current = pMap.at(current);
     }
-    path.push_back(vertex_mapping.at(source));
+    path.push_back(vertex_mapping.at(component_vertex_mapping.at(source)));
 
     // reverse as works back from beginning
     std::reverse(path.begin(), path.end());
@@ -116,6 +117,7 @@ std::vector<size_t> getPath(
 template <class T>
 std::vector<size_t> traverse_components(const ORFNodeMap& ORF_map,
                                        const std::vector<size_t>& vertex_mapping,
+                                       const std::vector<size_t>& component_vertex_mapping,
                                        const std::unordered_set<size_t>& vertex_list,
                                        const GeneGraph& g,
                                        const float& minimum_path_score,
@@ -151,13 +153,13 @@ std::vector<size_t> traverse_components(const ORFNodeMap& ORF_map,
     for (const auto& start : start_vertices)
     {
         // get start score
-        const float start_score = -std::get<4>(ORF_map.at(vertex_mapping.at(start)));
+        const float start_score = -std::get<4>(ORF_map.at(vertex_mapping.at(component_vertex_mapping.at(start))));
 
         // if starting node is highest scorer, set as path
         if (start_score < high_score)
         {
             high_score = start_score;
-            gene_path = {vertex_mapping.at(start)};
+            gene_path = {vertex_mapping.at(component_vertex_mapping.at(start))};
         }
 
         std::vector<float> distances(numVertices);
@@ -181,7 +183,7 @@ std::vector<size_t> traverse_components(const ORFNodeMap& ORF_map,
 
             float path_score = start_score;
 
-            auto path = getPath(g, pMap, distances, start, end, path_score, vertex_mapping);
+            auto path = getPath(g, pMap, distances, start, end, path_score, vertex_mapping, component_vertex_mapping);
 
             if (path_score < high_score)
             {
@@ -256,15 +258,14 @@ std::vector<std::vector<size_t>> call_true_genes (const ORFNodeMap& ORF_map,
 {
     std::vector<std::vector<size_t>> gene_paths;
 
-    GeneGraph tc;
-    std::vector<size_t> vertex_mapping;
-    std::unordered_map<size_t, size_t> ORF_ID_mapping;
-    std::vector<std::unordered_set<size_t>> components;
-    size_t numVertices;
     // scope for graph objects
     {
         // initialise graph
         GeneGraph g;
+        std::vector<size_t> vertex_mapping;
+        std::unordered_map<size_t, size_t> ORF_ID_mapping;
+        std::vector<std::unordered_set<size_t>> components;
+        size_t numVertices;
 
         // create IDs for vertex mappings
         int vertex_id = 0;
@@ -299,82 +300,112 @@ std::vector<std::vector<size_t>> call_true_genes (const ORFNodeMap& ORF_map,
             depth_first_search(g, visitor(vis));
         }
 
-
         // get components from simple graph
         components = get_components(g);
         numVertices = num_vertices(g);
 
-        // make transative closure graph
-        transitive_closure(g, tc);
-    }
-
-    // iterate over edges and add scores
-    std::vector<EdgeDescriptor> incompatible_edges;
-    auto es = boost::edges(tc);
-    for (auto eit = es.first; eit != es.second; ++eit)
-    {
-        // get sink and source and convert to ORF_IDs
-        const auto& source = vertex_mapping.at(boost::source(*eit, tc));
-        const auto& target = vertex_mapping.at(boost::target(*eit, tc));
-
-        float edge_score = std::get<4>(ORF_map.at(target));
-
-        if (overlap_map.find(target) == overlap_map.end())
+        // iterate over components using bellman ford algorithm
+        for (const auto& component : components)
         {
-            boost::put(boost::edge_weight_t(), tc, *eit, -edge_score);
-            continue;
-        } else if (overlap_map.at(target).find(source) == overlap_map.at(target).end())
-        {
-            boost::put(boost::edge_weight_t(), tc, *eit, -edge_score);
-            continue;
+            // make transative closure graph
+            GeneGraph tc;
+            // TODO this won't work with a component, need to generate a small graph as above most likely
+
+            std::vector<size_t> component_vertex_mapping;
+            std::unordered_map<size_t, size_t> component_ORF_ID_mapping;
+            {
+                GeneGraph component_g;
+                int component_vertex_id = 0;
+                for (const auto& entry : component)
+                {
+                    component_vertex_mapping.push_back(entry);
+                    component_ORF_ID_mapping[entry] = component_vertex_id++;
+                    add_vertex(component_g);
+                }
+
+                //iterate over component again and add edges if in original graph
+                for (const auto& source : component)
+                {
+                    for (const auto& target : component)
+                    {
+                        auto edgePair = boost::edge(source, target, g);
+                        if (edgePair.second)
+                        {
+                            add_edge(component_ORF_ID_mapping.at(source), component_ORF_ID_mapping.at(target), component_g);
+                        }
+                    }
+                }
+
+                transitive_closure(component_g, tc);
+            }
+
+            // iterate over edges and add scores
+            std::vector<EdgeDescriptor> incompatible_edges;
+            auto es = boost::edges(tc);
+            for (auto eit = es.first; eit != es.second; ++eit)
+            {
+                // get sink and source and convert to ORF_IDs
+                const auto& source = vertex_mapping.at(component_vertex_mapping.at(boost::source(*eit, tc)));
+                const auto& target = vertex_mapping.at(component_vertex_mapping.at(boost::target(*eit, tc)));
+
+                float edge_score = std::get<4>(ORF_map.at(target));
+
+                if (overlap_map.find(target) == overlap_map.end())
+                {
+                    boost::put(boost::edge_weight_t(), tc, *eit, -edge_score);
+                    continue;
+                } else if (overlap_map.at(target).find(source) == overlap_map.at(target).end())
+                {
+                    boost::put(boost::edge_weight_t(), tc, *eit, -edge_score);
+                    continue;
+                }
+
+                // parse the overlap type
+                const auto& overlap_pair = overlap_map.at(target).at(source);
+                const auto& overlap_type = overlap_pair.first;
+                const auto& abs_overlap = overlap_pair.second;
+
+                float penalty = 0;
+                if (overlap_type == 'i')
+                {
+                    incompatible_edges.push_back(*eit);
+                    continue;
+                } else if (overlap_type == 'u')
+                {
+                    penalty = unidirectional_penalty_per_base * abs_overlap;
+                } else if (overlap_type == 'c')
+                {
+                    penalty = convergent_penalty_per_base * abs_overlap;
+                } else if (overlap_type == 'd')
+                {
+                    penalty = divergent_penalty_per_base * abs_overlap;
+                }
+
+                // negate the penalty from edge_score
+                edge_score -= penalty;
+
+                // add edge score, making negative for bellman-ford
+                boost::put(boost::edge_weight_t(), tc, *eit, -edge_score);
+            }
+
+            // remove incompatible edges
+            for (const auto& e : incompatible_edges)
+            {
+                remove_edge(e, tc);
+            }
+
+            // get weight map for graph
+            const auto weight_pmap = get(boost::edge_weight_t(), tc);
+            
+            auto path = traverse_components(ORF_map, vertex_mapping, component_vertex_mapping, component, tc, minimum_path_score, numVertices, weight_pmap, head_kmer_arr);
+            // ensure path is not empty
+            if (!path.empty())
+            {
+                // need to convert to 
+                gene_paths.push_back(std::move(path));
+            }
         }
-
-        // parse the overlap type
-        const auto& overlap_pair = overlap_map.at(target).at(source);
-        const auto& overlap_type = overlap_pair.first;
-        const auto& abs_overlap = overlap_pair.second;
-
-        float penalty = 0;
-        if (overlap_type == 'i')
-        {
-            incompatible_edges.push_back(*eit);
-            continue;
-        } else if (overlap_type == 'u')
-        {
-            penalty = unidirectional_penalty_per_base * abs_overlap;
-        } else if (overlap_type == 'c')
-        {
-            penalty = convergent_penalty_per_base * abs_overlap;
-        } else if (overlap_type == 'd')
-        {
-            penalty = divergent_penalty_per_base * abs_overlap;
-        }
-
-        // negate the penalty from edge_score
-        edge_score -= penalty;
-
-        // add edge score, making negative for bellman-ford
-        boost::put(boost::edge_weight_t(), tc, *eit, -edge_score);
     }
 
-    // remove incompatible edges
-    for (const auto& e : incompatible_edges)
-    {
-        remove_edge(e, tc);
-    }
-
-    // get weight map for graph
-    const auto weight_pmap = get(boost::edge_weight_t(), tc);
-
-    // iterate over components using bellman ford algorithm
-    for (const auto& component : components)
-    {
-        auto path = traverse_components(ORF_map, vertex_mapping, component, tc, minimum_path_score, numVertices, weight_pmap, head_kmer_arr);
-        // ensure path is not empty
-        if (!path.empty())
-        {
-            gene_paths.push_back(std::move(path));
-        }
-    }
     return gene_paths;
 }
