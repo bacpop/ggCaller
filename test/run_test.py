c46af50383fad0595d76a940c81335434a96d3e5
Sam Horsfield <shorsfield@ebi.ac.uk>
2026-03-27 10:26:54 +0100

    Removes reference to panaroo scripts

--- test/run_test.py
+++ test/run_test.py
@@ -14,21 +14,9 @@ else:
     python_cmd = "python"
 
 ### reference build workflow ###
-sys.stderr.write("Running reference build workflow without annotation\n")
+sys.stderr.write("Running reference build workflow\n")
 subprocess.run(
-    python_cmd + " ../ggcaller-runner.py --refs pneumo_CL_group2.txt --kmer 31 --out test_dir --max-path-length 5000 --clean-mode strict --min-orf-length 100 --max-ORF-overlap 55 --alignment core --aligner def --annotation none --evalue 0.0001 --search-radius 3000",
-    shell=True,
-    check=True)
-
-sys.stderr.write("Running reference build workflow generating gff only\n")
-subprocess.run(
-    python_cmd + " ../ggcaller-runner.py --refs pneumo_CL_group2.txt --kmer 13 --out test_dir --max-path-length 5000 --clean-mode strict --min-orf-length 0 --max-ORF-overlap 55 --alignment core --aligner def --annotation fast --evalue 0.0001 --search-radius 3000 --save --gene-finding-only",
-    shell=True,
-    check=True)
-
-sys.stderr.write("Running reference build workflow with annotation\n")
-subprocess.run(
-    python_cmd + " ../ggcaller-runner.py --refs pneumo_CL_group2.txt --kmer 13 --out test_dir --max-path-length 5000 --clean-mode strict --min-orf-length 0 --max-ORF-overlap 55 --alignment core --aligner def --annotation fast --evalue 0.0001 --search-radius 3000 --save",
+    python_cmd + " ../ggcaller-runner.py --refs pneumo_CL_group2.txt --kmer 31 --out test_dir --max-path-length 5000 --min-orf-length 100 --max-ORF-overlap 55",
     shell=True,
     check=True)
 
@@ -56,38 +44,32 @@ subprocess.run(
 ### reference read workflow ###
 sys.stderr.write("Running reference read workflow\n")
 subprocess.run(
-    python_cmd + f" ../ggcaller-runner.py --graph pneumo_CL_group2.gfa --colours {color_file} --out test_dir --len-diff-cutoff 0.99 --alignment pan --aligner def --annotation sensitive --evalue 0.01 --core-threshold 0.96",
+    python_cmd + f" ../ggcaller-runner.py --graph pneumo_CL_group2.gfa --colours {color_file} --out test_dir --len-diff-cutoff 0.99",
     shell=True,
     check=True)
 
 ### read build workflow ###
 sys.stderr.write("Running read build workflow\n")
 subprocess.run(
-    python_cmd + " ../ggcaller-runner.py --reads pneumo_CL_group2.txt --kmer 31 --out test_dir --min-orf-score 150 --max-orf-orf-distance 5000 --identity-cutoff 0.99 --annotation ultrasensitive --clean-mode moderate --length-outlier-support-proportion 0.15 --min-edge-support-sv 1",
+    python_cmd + " ../ggcaller-runner.py --reads pneumo_CL_group2.txt --kmer 31 --out test_dir --min-orf-score 150 --max-orf-orf-distance 5000 --identity-cutoff 0.99",
     shell=True,
     check=True)
 
 ### reads + refs build workflow ###
 sys.stderr.write("Running reads + reference build workflow\n")
 subprocess.run(
-    python_cmd + " ../ggcaller-runner.py --refs pneumo_CL_group2.txt --reads pneumo_CL_group2.txt --out test_dir --repeat --family-threshold 0.75 --merge-paralogs --refind-prop-match 0.15 --edge-support-threshold 2",
-    shell=True,
-    check=True)
-
-sys.stderr.write("Running reads + reference build workflow generating gff only\n")
-subprocess.run(
-    python_cmd + " ../ggcaller-runner.py --refs pneumo_CL_group2.txt --reads pneumo_CL_group2.txt --out test_dir --repeat --family-threshold 0.75 --merge-paralogs --refind-prop-match 0.15 --gene-finding-only",
+    python_cmd + " ../ggcaller-runner.py --refs pneumo_CL_group2.txt --reads pneumo_CL_group2.txt --out test_dir --repeat",
     shell=True,
     check=True)
 
 ### reads read workflow ###
 sys.stderr.write("Running reads read workflow\n")
 subprocess.run(
-    python_cmd + f" ../ggcaller-runner.py --graph pneumo_CL_group2.gfa --colours {color_file} --not-ref --out test_dir --repeat --clean-mode sensitive --min-trailing-support 1 --trailing-recursive 1",
+    python_cmd + f" ../ggcaller-runner.py --graph pneumo_CL_group2.gfa --colours {color_file} --not-ref --out test_dir --repeat",
     shell=True,
     check=True)
 
-sys.stderr.write("Running reference build without panaroo\n")
+sys.stderr.write("Running reference build without clustering\n")
 subprocess.run(
     python_cmd + " ../ggcaller-runner.py --refs pneumo_CL_group2.txt --kmer 31 --out test_dir --max-path-length 5000 --min-orf-length 100 --repeat --no-clustering",
     shell=True,
@@ -96,7 +78,7 @@ subprocess.run(
 ### reference read workflow ###
 sys.stderr.write("Running reference read workflow without models\n")
 subprocess.run(
-    python_cmd + f" ../ggcaller-runner.py --graph pneumo_CL_group2.gfa --colours {color_file} --out test_dir --no-filter --len-diff-cutoff 0.99 --alignment pan --aligner def --annotation sensitive --truncation-threshold 0.7 --ignore-pseduogenes --no-variants",
+    python_cmd + f" ../ggcaller-runner.py --graph pneumo_CL_group2.gfa --colours {color_file} --out test_dir --no-filter --len-diff-cutoff 0.99",
     shell=True,
     check=True)
 
